d573d1176474ef120fb9153f3839cc9fa5c5ca1b
renau06 <renaupadhyay2022@u.northwestern.edu>
2022-02-12 15:15:18 -0600

    sign in/sign out buton

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import {SignInButton} from "./components/users"
+import Login from "./pages/Login";
+import {useEffect, useState} from 'react';
+import {useUserState} from "./database/users";
 
 function App() {
+  const [uid, setUid] = useState("")
+  const [UEmail, setUEmail] = useState("");
+  const [UName, setUName] = useState("");
+  const [user] = useUserState({setUEmail, setUName, setUid});
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Welcome to Fountain!
         </p>
-        <SignInButton />
+        <Login user={user} UEmail={UEmail} UName={UName}
+                                                     setUEmail={setUEmail} setUName={setUName} setUid={setUid}
+                                                     
+                />
       </header>
     </div>
   );
