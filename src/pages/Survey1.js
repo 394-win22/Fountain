4a538b66feb3574d52160db7b584eb55805dbac0
renau06 <renaupadhyay2022@u.northwestern.edu>
2022-02-12 16:20:31 -0600

    style survey page 1

--- src/pages/Survey1.js
+++ src/pages/Survey1.js
@@ -9,22 +9,23 @@ function Survey1({ user, UEmail, setUEmail, UName, setUName, setUid, question}) 
     return (
         <div>
             <h1> Hi {UName}! {question}</h1>
-            <Card className="m-2" onClick={ () => {
+            <Card className="feeling" onClick={ () => {
                     set_feeling("happy", UName, UEmail)
                     navigate('/survey2');
+                    
                     }}>
                 <Card.Body>
                     <Card.Title><i class="bi bi-emoji-smile"></i></Card.Title>
                 </Card.Body>
             </Card>
-            <Card className="m-2" onClick={ () => {
+            <Card className="feeling" onClick={ () => {
                     set_feeling("medium", UName, UEmail)
                     navigate('/survey2');}}>
                 <Card.Body>
                     <Card.Title><i class="bi bi-emoji-neutral"></i></Card.Title>
                 </Card.Body>
             </Card>
-            <Card className="m-2" onClick={ () => {
+            <Card className="feeling" onClick={ () => {
                     set_feeling("sad", UName, UEmail)
                     navigate('/survey2');}}>
                 <Card.Body>
