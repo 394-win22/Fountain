6c952fdb4fb2b23023ace329a3ce8de67b28e2d3
renau06 <renaupadhyay2022@u.northwestern.edu>
2022-02-19 16:49:50 -0600

    timer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,31 @@ import {useNavigate} from 'react-router-dom';
 import {SignInButton, SignOutButton} from "../components/users";
 import {fetch_workouts} from "../database/workout";
 import {useEffect, useState} from 'react';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 
+const UrgeWithPleasureComponent = ({playing}) => {
+   return (<CountdownCircleTimer
+      isPlaying={playing}
+      duration={600}
+      colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+      colorsTime={[7, 5, 2, 0]}
+    >
+      {({ remainingTime }) =>  {
+        const minutes = Math.floor(remainingTime / 60);
+        let seconds = remainingTime % 60;
+        if (seconds <10){
+            seconds = "0"+seconds;
+        }
+        return `${minutes}:${seconds}`}
+        }
+    </CountdownCircleTimer>)
+}
 
 function Home({ UEmail, setUEmail, UName, setUName}) {
     const navigate = useNavigate();
     const [workouts, setWorkouts] = useState([]);
-
+    const [playing, setPlaying] = useState(false);
+    const [remainingTime, setRemaingTime] = useState();
     useEffect(() => {
         fetch_workouts().then(value => {
             let random = value.sort(() => .5 - Math.random()).slice(0,5);
@@ -21,18 +40,18 @@ function Home({ UEmail, setUEmail, UName, setUName}) {
 
     const Workout = () => {
         return(
-        workouts.map(workout => <div key={workout["Index"] }>{workout} </div>
+        workouts.map(workout => <div key={workout["Index"] }>{workout} 2m </div>
         )) 
     }
     
-
     return (
         <div className="home-wrapper">
         <Workout />
+        {playing? <button onClick={() => setPlaying(false)}>Pause</button>: <button onClick={() => setPlaying(true)}>Start</button>}
+        <UrgeWithPleasureComponent playing={playing}/>
         <SignOutButton setUEmail={setUEmail} setUName={setUName} />
         </div>
-    )  
+    );  
 }
 
 export default Home;
-
