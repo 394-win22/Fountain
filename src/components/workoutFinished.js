82f04e8ff5653c9fd4c1c07d12bebff1c4f23d89
renau06 <renaupadhyay2022@u.northwestern.edu>
2022-02-22 17:48:42 -0600

    style workout finished page

--- src/components/workoutFinished.js
+++ src/components/workoutFinished.js
@@ -2,9 +2,9 @@
 export function WorkoutFinished() {
     const today = new Date(Date.now());
     return (
-        <div>
-            <h2> Congratulations!You finished!</h2>
-            <button onClick={() => {
+        <div className="workout-finished">
+            <h2 className="workout-finished-header"> Congratulations! You finished!</h2>
+            <button type="button" className="btn btn-outline-dark"  onClick={() => {
                 navigator.clipboard.writeText("I finished the fountain workout on " + today.toDateString() + "!")
                 alert("copy success!")
             }} >
