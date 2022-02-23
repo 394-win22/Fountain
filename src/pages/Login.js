907cd33290ea2c68f5169693c575cbce71f6df10
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2022-02-23 16:42:43 -0600

    testing dev server

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,16 +2,16 @@ import React from "react";
 import {SignInButton, SignOutButton} from "../components/users";
 
 function Login({ user, UEmail, setUEmail, UName, setUName, setUid}) {
-    
+
     return (
         <div className="login-wrapper">
             <div className="intro-and-button">
-                <h1 className="intro" style={{fontFamily:"Fredericka the AfterSurvey"}} >Welcome to Fountain</h1>
+                <h1 className="intro" style={{fontFamily:"Fredericka the AfterSurvey"}} >UNWelcome to Fountain</h1>
                 { user ? <SignOutButton setUEmail={setUEmail} setUName={setUName} setUid={setUid}/> :
                         <SignInButton setUEmail={setUEmail} setUName={setUName} /> }
-                    
+
                 <div className="container">
-                    <div className="row">      
+                    <div className="row">
                         <div className="col-sm-6">
                             {user? <div>Your Name: {UName ? UName : "NULL"}</div> : null}
                             {user? <div>Your email: {UEmail ? UEmail : "NULL"}</div> : null}
