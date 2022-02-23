21a8d0ac7476744a452ca1ed6b02346b19752586
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2022-02-23 16:24:10 -0600

    correcting the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Welcome to Fountain/i);
+  expect(linkElement).toBeInTheDocument();
+});
