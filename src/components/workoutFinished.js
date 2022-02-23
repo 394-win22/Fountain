3a439c5cf0d4e8ce45c4cf92414e49d14f7326b9
renau06 <renaupadhyay2022@u.northwestern.edu>
2022-02-22 18:03:55 -0600

    add link to share

--- src/components/workoutFinished.js
+++ src/components/workoutFinished.js
@@ -5,7 +5,7 @@ export function WorkoutFinished() {
         <div className="workout-finished">
             <h2 className="workout-finished-header"> Congratulations! You finished!</h2>
             <button type="button" className="btn btn-outline-dark"  onClick={() => {
-                navigator.clipboard.writeText("I finished the fountain workout on " + today.toDateString() + "!")
+                navigator.clipboard.writeText("I finished the fountain workout on " + today.toDateString() + "! https://fountain-37243.web.app")
                 alert("copy success!")
             }} >
                 copy to clipboard
