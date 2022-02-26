5ad5bbe99bedc8af1a440a3c79fa25b9ea5720b8
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2022-02-26 17:06:44 -0600

    changed finished workout screen

--- src/components/workoutFinished.js
+++ src/components/workoutFinished.js
@@ -1,16 +1,71 @@
 
+import React from "react";
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {get_user} from "../database/users";
+
 export function WorkoutFinished() {
     const today = new Date(Date.now());
+    const { uid } = useParams();
+    const [name, setName] = useState("");
+    const [photo, setPhoto] = useState("");
+  
+    useEffect(() => {
+        get_user(uid).then(value => {
+            setName(value.val().userName)
+            setPhoto(value.val().userPhoto)
+        });
+    }, [uid]);
     return (
-        <div className="workout-finished">
-            <h2 className="workout-finished-header"> Congratulations! You finished!</h2>
-            <button type="button" className="btn btn-outline-dark"  onClick={() => {
-                navigator.clipboard.writeText("I finished the fountain workout on " + today.toDateString() + "! https://fountain-37243.web.app")
-                alert("copy success!")
+        <div className="startwrapper">
+          
+          <div>You did it!</div>
+          <img className = "profile-photo" src={photo} alt="UserPhoto"/>
+          <div>Well done, youe're on track to improving your lifespan by an extra 5 years!</div>
+          <img src="https://firebasestorage.googleapis.com/v0/b/fountain-37243.appspot.com/o/badge1.jpg?alt=media&token=77ce3782-3428-4f95-ba5e-1567661136dc"alt="badge working towards"/>
+          <a className="startbutton" href="https://www.facebook.com/groups/fountainfit/">Share on Fountain Facebook</a>
+          <button type="button" className="btn btn-outline-dark"  onClick={() => {
+                alert("This is a premium feature!")
             }} >
-                copy to clipboard
+                Try a workout with a friend
             </button>
-            <div><a href="https://www.facebook.com/groups/fountainfit/"> Share on Fountain Facebook </a></div>
+          
         </div>
     );
 }
+
+function Start() {
+    const { uid } = useParams();
+    const [name, setName] = useState("");
+    const [photo, setPhoto] = useState("");
+  
+    useEffect(() => {
+        get_user(uid).then(value => {
+            setName(value.val().userName)
+            setPhoto(value.val().userPhoto)
+        });
+    }, [uid]);
+    return (
+      <>
+        <div className="startwrapper">
+          
+          <div>Welcome back {name ? name : "NULL"}!</div>
+          <img className = "profile-photo" src={photo} alt="UserPhoto"/>
+          <div>Workout Of The Day Challenge (WODC)</div>
+  
+          <a className="startbutton" href={"/countdown/"+uid}>Begin Workout of the Day</a>
+          <div className="card m-1 p-2 startwrapper"> 
+            <div>
+              Complete this class and earn your two week acheivement badge!
+            </div>
+            <div>
+              <img src="https://firebasestorage.googleapis.com/v0/b/fountain-37243.appspot.com/o/badge1.jpg?alt=media&token=77ce3782-3428-4f95-ba5e-1567661136dc"alt="badge working towards"/>
+            </div>
+            <a href={"/preview/"+uid}>Review Workout</a>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default Start;
