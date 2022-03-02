d354385e3e36ab69d3457810424d174b84530d02
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2022-03-02 16:17:09 -0600

    testing pressing the login button

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -1,8 +1,15 @@
 describe ('Test App', () => {
 
-    it ('launches', () => {
-      cy.visit ('/');
-    });
+  it ('launches', () => {
+    cy.visit ('/');
   });
-  
-  
+
+  it ('opens Welcome to Fountain', () => {
+    cy.visit ('/');
+    cy.get('[data-cy=Login]').should('contain', 'Welcome to Fountain');
+  });
+  it('press the login button', () => {
+    cy.visit ('/');
+    cy.get('[data-cy=button]').click();
+  });
+});
