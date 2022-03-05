f2d7757401485f7c88610a7e1b5717d875b3b573
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2022-03-05 14:43:12 -0600

    changes that dont work

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,12 @@ const admin = require("firebase-admin");
 
 // Initialize Firebase app for database access
 admin.initializeApp();
+const db = admin.firestore()
 
 exports.scheduledFunctionCrontab = functions.pubsub.schedule("45 14 * * *")
     .timeZone("America/Chicago")
     .onRun((context) => {
-    const writeResult = await admin.firestore().collection('messages').add({to: "+17088343108", from: "+18623622582", body: "testing if message sent from fountain" });
+    const writeResult = await db.collection('messages').add({to: "+17088343108", from: "+18623622582", body: "testing if message sent from fountain" });
       console.log("This sent a message!!");
       return null;
     });
