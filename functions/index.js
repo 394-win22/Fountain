a28b8a0ec329bc6d0cb470e6251344b318c5ab96
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2022-03-05 14:40:59 -0600

    send message

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,11 @@ const admin = require("firebase-admin");
 // Initialize Firebase app for database access
 admin.initializeApp();
 
-exports.scheduledFunctionCrontab = functions.pubsub.schedule("25 14 * * *")
+exports.scheduledFunctionCrontab = functions.pubsub.schedule("45 14 * * *")
     .timeZone("America/Chicago")
     .onRun((context) => {
-      console.log("This will be run every day at 11:05 AM Eastern!");
+    const writeResult = await admin.firestore().collection('messages').add({to: "+17088343108", from: "+18623622582", body: "testing if message sent from fountain" });
+      console.log("This sent a message!!");
       return null;
     });
 
