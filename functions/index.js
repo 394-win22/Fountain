c11a35c4e96e4e4137a8b91d92b3aa3f709ebd9f
Conor Kotwasinski <conorkotwasinski2024@u.northwestern.edu>
2022-03-05 14:23:08 -0600

    chagned spaces

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,11 @@ const admin = require("firebase-admin");
 // Initialize Firebase app for database access
 admin.initializeApp();
 
-exports.scheduledFunctionCrontab = functions.pubsub.schedule("18 14 * * *")
+exports.scheduledFunctionCrontab = functions.pubsub.schedule("25 14 * * *")
     .timeZone("America/Chicago")
     .onRun((context) => {
-        console.log("This will be run every day at 11:05 AM Eastern!");
-        return null;
-});
+      console.log("This will be run every day at 11:05 AM Eastern!");
+      return null;
+    });
 
 
