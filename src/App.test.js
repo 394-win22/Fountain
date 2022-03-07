22ef0e47ca4bfb1cf6ec302713b0d66eac479cf9
Dong Yang <outskidding@gmail.com>
2022-03-07 16:47:13 -0600

    add npm test script

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import {Profile} from "./pages/Profile";
+import {useParams} from 'react-router-dom';
 
-test('renders learn react link', () => {
+jest.mock('../node_modules/react-router-dom');
+
+const mockUser = {
+  "uid": "0DiifPJmD5fXTgfyjMrpjVWFW3U2"
+};
+
+test('renders the welcome page', () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome to Fountain/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders the profile page', () => {
+  useParams.mockReturnValue(mockUser);
+  render(<Profile />);
+  const linkElement = screen.getByText(/Alejandro Malavet/i);
   expect(linkElement).toBeInTheDocument();
 });
