372b3f46d50508ff628b6b8c85359f0592de25dd
renau06 <renaupadhyay2022@u.northwestern.edu>
2022-03-08 17:08:52 -0600

    change text every 5 seconds

--- src/components/workoutArea.js
+++ src/components/workoutArea.js
@@ -12,18 +12,79 @@ function displayMessage (remTime, messages){
     let message = null;
     if (messages) {
         messages = Object.values(messages)
-        if (remTime <= 120 && remTime > 110 ){
+        if (remTime <= 120 && remTime > 115 ){
             message = messages[0]
         }
-        else if (remTime <= 100 && remTime > 50){
+        else if (remTime <= 115 && remTime > 110){
             message = messages[1]
         }
-        else if (remTime <= 30 && remTime > 20){
+        else if (remTime <= 110 && remTime > 105){
             message = messages[2]
         }
-        else if (remTime === 0){
-            message = messages[3]
+        else if (remTime <= 105 && remTime > 100){
+            message = messages[0]
         }
+        else if (remTime <= 100 && remTime > 95){
+            message = messages[1]
+        }
+        else if (remTime <= 95 && remTime > 90){
+            message = messages[2]
+        }
+        else if (remTime <= 90 && remTime > 85){
+            message = messages[0]
+        }
+        else if (remTime <= 85 && remTime > 80){
+            message = messages[1]
+        }
+        else if (remTime <= 80 && remTime > 75){
+            message = messages[2]
+        }
+        else if (remTime <= 75 && remTime > 70){
+            message = messages[0]
+        }
+        else if (remTime <= 70 && remTime > 65){
+            message = messages[1]
+        }
+        else if (remTime <= 65 && remTime > 60){
+            message = messages[2]
+        }
+        else if (remTime <= 60 && remTime > 55){
+            message = messages[0]
+        }
+        else if (remTime <= 55 && remTime > 50){
+            message = messages[1]
+        }
+        else if (remTime <= 50 && remTime > 45){
+            message = messages[2]
+        }
+        else if (remTime <= 45 && remTime > 40){
+            message = messages[0]
+        }
+        else if (remTime <= 40 && remTime > 35){
+            message = messages[1]
+        }
+        else if (remTime <= 35 && remTime > 30){
+            message = messages[2]
+        }
+        else if (remTime <= 30 && remTime > 25){
+            message = messages[0]
+        }
+        else if (remTime <= 25 && remTime > 20){
+            message = messages[1]
+        }
+        else if (remTime <= 20 && remTime > 15){
+            message = messages[2]
+        }
+        else if (remTime <= 15 && remTime > 10){
+            message = messages[0]
+        }
+        else if (remTime <= 10 && remTime > 5){
+            message = messages[1]
+        }
+        else if (remTime <= 5 && remTime > 0){
+            message = messages[2]
+        }
+
         return (
             <div>
                 {message}
