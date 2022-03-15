25d98368e6c528a4da6da8583c684ef3a8c2d456
Dong Yang <outskidding@gmail.com>
2022-03-15 16:39:26 -0500

    update env

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import {firebaseConfig} from "../../ENV";
+import {firebaseConfig} from "../ENV";
 
 // Initialize Firebase
 export const firebase = initializeApp(firebaseConfig);
