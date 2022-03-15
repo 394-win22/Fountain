7819460d40586f5e8a5300dc9c9d91e6faa9b065
renau06 <renaupadhyay2022@u.northwestern.edu>
2022-03-14 20:48:15 -0400

    start workout tests

--- cypress/integration/StartWorkout.spec.js
+++ cypress/integration/StartWorkout.spec.js
@@ -14,14 +14,16 @@ describe('Visit Workout Page', ()=> {
         cy.get('[data-cy=countdown-timer]').should('contain', `Are you ready?!`);
     })
 
-    it('Gif on workout page', () => {
+    it('Wait for countdown page', () => {
         cy.wait(300)
         cy.get('[data-cy=gif]').should('be.visible');   
     })
 
-    it('Gif on workout page', () => {
-        cy.wait(300)
-        cy.get('[data-cy=gif]').should('be.visible');   
+    it('Workout Page', () => {
+        cy.get('[data-cy=gif]').should('be.visible');
+        cy.get('[data-cy=pause]').should('be.visible');
+        cy.get('[data-cy=exercise-number]').should('contain', `Exercise`); 
+
     })
 
 
